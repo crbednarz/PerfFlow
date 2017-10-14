c7873bb3a77120e5c3f189b5f6958a8daaa679bf
Skiphs <cr.bednarz@gmail.com>
2017-10-14 14:39:09 -0400

    Instruction pointer can now be retrieved from StackFrame

--- PerfFlow/src/sampling/StackFrame.h
+++ PerfFlow/src/sampling/StackFrame.h
@@ -11,9 +11,17 @@ public:
 	StackFrame();
 	explicit StackFrame(size_t instructionPointer);
 
+	size_t instructionPointer() const;
+
 private:
 	size_t _instructionPointer;
 };
 
 
+}
+
+
+inline size_t PerfFlow::StackFrame::instructionPointer() const
+{
+	return _instructionPointer;
 }
