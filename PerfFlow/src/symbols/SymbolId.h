5783a9278df0e583fd99afb1b6499936367f9a9b
Skiphs <cr.bednarz@gmail.com>
2017-10-17 19:47:52 -0400

    SymbolId is now hashable and comparable.

--- PerfFlow/src/symbols/SymbolId.h
+++ PerfFlow/src/symbols/SymbolId.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include <functional>
 
 
 namespace PerfFlow
@@ -10,6 +11,8 @@ class SymbolId
 {
 public:
 	explicit SymbolId(size_t symbolAddress);
+	bool operator ==(const SymbolId &rhs) const;
+	bool operator !=(const SymbolId &rhs) const;
 
 	size_t address() const;
 
@@ -20,11 +23,35 @@ private:
 
 }
 
+namespace std
+{
+	template<> 
+	struct hash<PerfFlow::SymbolId>
+	{
+		size_t operator()(const PerfFlow::SymbolId& value) const noexcept
+		{
+			return hash<size_t>{}(value.address());
+		}
+	};
+}
+
 
 inline PerfFlow::SymbolId::SymbolId(size_t symbolAddress) :
 	_address(symbolAddress)
 {
 
+}
+
+
+inline bool PerfFlow::SymbolId::operator==(const SymbolId& rhs) const
+{
+	return _address == rhs._address;
+}
+
+
+inline bool PerfFlow::SymbolId::operator!=(const SymbolId& rhs) const
+{
+	return !(*this == rhs);
 }
 
 
