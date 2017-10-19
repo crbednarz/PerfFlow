782f4f2948e8b44eb308b54d770892f7b739c1ad
Skiphs <cr.bednarz@gmail.com>
2017-10-18 23:13:59 -0400

    Added empty SamplingTask unit test

---
+++ PerfFlowTests/tests/sampling/SamplingTaskTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "sampling/SamplingTask.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace PerfFlow;
+
+namespace PerfFlowTests
+{
+
+
+	TEST_CLASS(SamplingTaskTests)
+	{
+
+	};
+
+
+}
