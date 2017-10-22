2abe1edc0f6d94229fb8005244b1bf173a705cb9
Skiphs <cr.bednarz@gmail.com>
2017-10-21 21:24:59 -0400

    VisualizerPane now correctly initializes the OpenGL context.

--- PerfFlowUI/src/ui/VisualizerPane.cpp
+++ PerfFlowUI/src/ui/VisualizerPane.cpp
@@ -11,16 +11,42 @@ wxEND_EVENT_TABLE()
 VisualizerPane::VisualizerPane(wxFrame* parent) :
 	wxGLCanvas(parent)
 {
+
+	wxGLContextAttrs contextAttributes;
+	contextAttributes.PlatformDefaults().OGLVersion(3, 2).EndList();
+	auto context = std::make_unique<wxGLContext>(this, nullptr, &contextAttributes);
+
+	if (!context->IsOK())
+	{
+		wxLogFatalError("Failed to initialize OpenGL context.");
+		return;
+	}
+
+	_context = std::move(context);
+	SetCurrent(*_context);
+
+	if (!gladLoadGL())
+	{
+		wxLogFatalError("Unable to initialize OpenGL.");
+		return;
+	}
+
 }
 
 
 void VisualizerPane::onPaint(wxPaintEvent& paintEvent)
 {
+	oglplus::Context gl;
+	wxPaintDC dc(this);
+
+	gl.ClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+	gl.Clear().ColorBuffer();
+
+	SwapBuffers();
 }
 
 
 void VisualizerPane::onSize(wxSizeEvent& sizeEvent)
 {
-	wxLogMessage("test?");
 
 }
