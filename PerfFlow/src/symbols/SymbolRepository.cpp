940648487238e205267dab4e7d96f6e95ee57c4b
Skiphs <cr.bednarz@gmail.com>
2017-10-24 19:20:09 -0400

    Added locking to SymbolRepository to ensure thread safety.

--- PerfFlow/src/symbols/SymbolRepository.cpp
+++ PerfFlow/src/symbols/SymbolRepository.cpp
@@ -5,13 +5,17 @@
 
 void PerfFlow::SymbolRepository::addSymbol(SymbolId id, const Symbol& symbol)
 {
-	assert(!hasSymbol(id));
-	_symbols.insert(std::make_pair(id, symbol));
+	if (!hasSymbol(id))
+	{
+		std::lock_guard<std::mutex> lockGuard(_mutex);
+		_symbols.insert(std::make_pair(id, symbol));
+	}
 }
 
 
-const PerfFlow::Symbol* PerfFlow::SymbolRepository::tryGetSymbol(SymbolId id) const
+const PerfFlow::Symbol* PerfFlow::SymbolRepository::tryGetSymbol(SymbolId id)
 {
+	std::lock_guard<std::mutex> lockGuard(_mutex);
 	auto it = _symbols.find(id);
 
 	if (it != _symbols.end())
@@ -21,13 +25,15 @@ const PerfFlow::Symbol* PerfFlow::SymbolRepository::tryGetSymbol(SymbolId id) co
 }
 
 
-bool PerfFlow::SymbolRepository::hasSymbol(SymbolId id) const
+bool PerfFlow::SymbolRepository::hasSymbol(SymbolId id)
 {
+	std::lock_guard<std::mutex> lockGuard(_mutex);
 	return _symbols.find(id) != _symbols.end();
 }
 
 
-size_t PerfFlow::SymbolRepository::totalSymbols() const
+size_t PerfFlow::SymbolRepository::totalSymbols()
 {
+	std::lock_guard<std::mutex> lockGuard(_mutex);
 	return _symbols.size();
 }
--- PerfFlow/src/symbols/SymbolRepository.h
+++ PerfFlow/src/symbols/SymbolRepository.h
@@ -2,6 +2,7 @@
 #include "SymbolId.h"
 #include "Symbol.h"
 #include <unordered_map>
+#include <mutex>
 
 
 namespace PerfFlow
@@ -16,13 +17,14 @@ public:
 
 	void addSymbol(SymbolId id, const Symbol& symbol);
 
-	const Symbol* tryGetSymbol(SymbolId id) const;
-	bool hasSymbol(SymbolId id) const;
+	const Symbol* tryGetSymbol(SymbolId id);
+	bool hasSymbol(SymbolId id);
 	
-	size_t totalSymbols() const;
+	size_t totalSymbols();
 
 private:
 	std::unordered_map<SymbolId, Symbol> _symbols;
+	std::mutex _mutex;
 
 };
 
